56e5d5b5e9c73048e156ac32c7ba9a8d2ec0dc6b
Michael Peter <michael_piterct@hotmail.com>
2021-05-19 22:35:03 -0300

    BaseController

---
+++ Notifications.Service/Notifications.Service.API/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notifications.Service.Shared.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notifications.Service.API.Controllers
+{
+    public class BaseController : Controller
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult GetResult(GenericCommandResult result) => StatusCode(result.StatusCode, result);
+    }
+}
---
+++ Notifications.Service/Notifications.Service.Shared/Commands/GenericCommandResult.cs
@@ -0,0 +1,26 @@
+﻿using Flunt.Notifications;
+using Notifications.Service.Shared.Commands.Contracts;
+using System.Collections.Generic;
+
+namespace Notifications.Service.Shared.Commands
+{
+    public class GenericCommandResult : ICommandResult
+    {
+        public GenericCommandResult() { }
+
+        public GenericCommandResult(bool success, string message, object data, int statusCode, IEnumerable<Notification> notifications)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+            StatusCode = statusCode;
+            Notifications = notifications;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+        public int StatusCode { get; set; }
+        public IEnumerable<Notification> Notifications { get; set; }
+    }
+}
