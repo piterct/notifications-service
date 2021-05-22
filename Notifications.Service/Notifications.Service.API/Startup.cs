4573368cb1385981fe258179bab6dbe5e0f7f2f7
Michael Peter <michael_piterct@hotmail.com>
2021-05-22 13:14:23 -0300

    DependencyRegister

--- Notifications.Service/Notifications.Service.API/Startup.cs
+++ Notifications.Service/Notifications.Service.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Notifications.Service.CrossCutting.DependencyInjectionConfig;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,14 @@ namespace Notifications.Service.API
                     Title = "Notifications"
                 });
             });
+
+            ConfigureDI(services, Configuration);
+        }
+
+        public static void ConfigureDI(IServiceCollection services, IConfiguration configuration)
+        {
+            //sign the services to di container   
+            DependencyRegister.AddScoped(services, configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Notifications.Service/Notifications.Service.CrossCutting/DependencyInjectionConfig/DependencyRegister.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notifications.Service.Domain.Handlers;
+
+namespace Notifications.Service.CrossCutting.DependencyInjectionConfig
+{
+    public static class DependencyRegister
+    {
+        public static void AddScoped(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Handlers
+            services.AddTransient<NotificationHandler, NotificationHandler>();
+            #endregion
+        }
+    }
+}
