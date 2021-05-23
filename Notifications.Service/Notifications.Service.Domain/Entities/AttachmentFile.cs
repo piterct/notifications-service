9d8ee40a1d78ce98286baddfb8ebfeaf5c5a38d4
Michael Peter <michael_piterct@hotmail.com>
2021-05-23 03:14:29 -0300

    AttachmentFile

---
+++ Notifications.Service/Notifications.Service.Domain/Entities/AttachmentFile.cs
@@ -0,0 +1,9 @@
+﻿namespace Notifications.Service.Domain.Entities
+{
+    public class AttachmentFile
+    {
+        public string FileName { get; set; }
+        public string Base64 { get; set; }
+        public string FilePath { get; set; }
+    }
+}
