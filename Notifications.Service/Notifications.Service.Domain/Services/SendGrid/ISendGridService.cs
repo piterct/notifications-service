88b9ccd86c4e6c8901eee74be752c191cfe4697d
Michael Peter <michael_piterct@hotmail.com>
2021-05-27 23:00:05 -0300

    Interface SendGridService

---
+++ Notifications.Service/Notifications.Service.Domain/Services/SendGrid/ISendGridService.cs
@@ -0,0 +1,11 @@
+﻿using Notifications.Service.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notifications.Service.Domain.Services.SendGrid
+{
+    public interface ISendGridService
+    {
+        ValueTask<bool> SendEmailSendGrid(string destination, string title, string body, string cc = "", string cco = "", List<AttachmentFile> attachmentPathList = null);
+    }
+}
