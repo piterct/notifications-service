b4216e7ba3f9bf7fa4afd0ec8128705ca6b67fd0
Michael Peter <michael_piterct@hotmail.com>
2021-05-30 01:33:03 -0300

    Input Sms Command

---
+++ Notifications.Service/Notifications.Service.Domain/Commands/Inputs/PostSendSmsCommmandInput.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Notifications.Service.Shared.Commands.Contracts;
+
+namespace Notifications.Service.Domain.Commands.Inputs
+{
+    public class PostSendSmsCommmandInput : Notifiable, ICommand
+    {
+        public string To { get; set; }
+        public string Message { get; set; }
+
+        public void Validate()
+        {
+
+            AddNotifications(
+           new Contract()
+               .Requires()
+               .IsNotNullOrEmpty(To, "To", "This value is not valid!")
+               .IsNotNullOrEmpty(Message, "Message", "This value is not valid!")
+                );
+        }
+    }
+}
